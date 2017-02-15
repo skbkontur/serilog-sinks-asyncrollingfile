a928f8c9c4d3301f0b88470633aacbda1e9a8d3b
Yury Pliner <yury.pliner@gmail.com>
2017-02-15 13:46:18 +0500

    Initial commit

---
+++ test/Serilog.Sinks.AsyncRollingFile.Tests/AsyncRollingFileTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+
+namespace Serilog.Sinks.ElkStreams.Tests
+{
+    public class AsyncRollingFileTests
+    {
+        [Fact]
+        public void Send()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.AsyncRollingFile("{Date}.log")
+                .CreateLogger();
+
+            Log.Logger.Debug("Hello, {World}!", "world");
+            Log.Logger.Debug(new Exception(), "Hello, {World}!", "world");
+            Log.Logger.Information("Hello, {World}!", "world");
+            Log.Logger.Information(new Exception(), "Hello, {World}!", "world");
+            Log.Logger.Warning("Hello, {World}!", "world");
+            Log.Logger.Warning(new Exception(), "Hello, {World}!", "world");
+            Log.Logger.Error("Hello, {World}!", "world");
+            Log.Logger.Error(new Exception(), "Hello, {World}!", "world");
+            Log.Logger.Fatal("Hello, {World}!", "world");
+            Log.Logger.Fatal(new Exception(), "Hello, {World}!", "world");
+
+            Log.CloseAndFlush();
+        }
+    }
+}
